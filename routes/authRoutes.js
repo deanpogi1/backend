1961e93a85f57b1a0cea15056e9bfe0bc80864a8
deanpogi1 <dpacardo@gbox.ncf.edu.ph>
2024-10-14 18:45:42 +0800

    update route/authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,9 @@
-// routes/authRoutes.js
-
 const express = require('express');
-const authcontroller = require('../controllers/authcontroller'); // Ensure this matches the actual file name
-const authenticateToken = require('../middlewares/authMiddleware'); // Include your middleware if needed
+const { register, login } = require('../controllers/authcontroller');
 
-const router = express.Router();
+const router = express.Router ();
 
-// Registration route
-router.post('/register', authcontroller.register); // Reference the correct function
-// Login route
-router.post('/login', authcontroller.login); // Reference the correct function
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
